b4f56cf47662fa5b27060b870fdb4f9a02324e39
getaddrinfo <53128384+getaddrinfo@users.noreply.github.com>
2020-06-20 19:22:45 +0100

    Remove unnecessary code (#1)
    * Delete structs.go
    
    * Update main.go

--- main.go
+++ main.go
@@ -1,41 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
+import "net/http"
 
 func main() {
 	http.Handle("/cdn/", http.StripPrefix("/cdn/", http.FileServer(http.Dir("./cdn"))))
-	http.HandleFunc("/api/v1/status", status)
 	http.ListenAndServe(":8000", nil)
-}
-
-func status(w http.ResponseWriter, r *http.Request) {
-	shards := []Shard{
-		Shard{ID: 1, Guilds: 1000, ExpectedGuilds: 1002, StatusText: "No Issues", StatusCode: 0},
-		Shard{ID: 2, Guilds: 1000, ExpectedGuilds: 1002, StatusText: "No Issues", StatusCode: 0},
-		Shard{ID: 3, Guilds: 1000, ExpectedGuilds: 1002, StatusText: "No Issues", StatusCode: 0},
-	}
-
-	statuses := []Status{
-		Status{
-			"API",
-			"No Issues",
-			0,
-		},
-	}
-
-	header := Header{
-		Info:     "no issues",
-		Statuses: statuses,
-	}
-
-	json.NewEncoder(w).Encode(struct {
-		Header Header  `json:"header"`
-		Shards []Shard `json:"shards"`
-	}{
-		Header: header,
-		Shards: shards,
-	})
 }
